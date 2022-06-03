58c2accdda10745c9c9df8c765157b9c147c3f30
Lucas Vitor <vitorvicentelucas@gmail.com>
2022-06-03 00:13:59 -0300

    js adicionado

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function calcular(t1 = 0, t2 = 0, t3 = 0) {
+let inputA = document.querySelector('.a')
+let inputB = document.querySelector('.b')
+let inputC = document.querySelector('.c')
+let calcBtn = document.querySelector('.calcBtn')
+
+calcBtn.addEventListener('click', calcular)
+
+function calcular() {
+    ponto(inputA.value, inputB.value, inputC.value)
+    limparCampo()
+}
+
+function ponto(t1=0, t2=0, t3=0) {
     let a = (t1 / 2)
     let b = (t2 / 2)
     let c = (t3 * (-1)) + (Math.pow(a, 2)) + (Math.pow(b, 2))
@@ -12,15 +24,26 @@ function calcular(t1 = 0, t2 = 0, t3 = 0) {
     }
 
     let pontos = `(${a}, ${b})`
+    raio(c, pontos)
+    return pontos
+}
 
+function raio(c, pontos) {
     let raio = Math.sqrt(c)
 
     if (raio % 1 !== 0) {
         raio = `√${c}`
     }
-
-    return `Pontos: ${pontos} ; raio: ${raio}`
+    resultado(pontos, raio)
+    return raio
 }
 
-console.log(calcular(-6, -2, -6))
+function resultado(pontos, raio) {
+    const areaResultado = document.querySelector('.resultado')
+    areaResultado.textContent = `Pontos: ${pontos} ; Raio: ${raio}` 
+    console.log()
+}
 
+function limparCampo() {
+    inputA.value = inputB.value = inputC.value = ''
+}
